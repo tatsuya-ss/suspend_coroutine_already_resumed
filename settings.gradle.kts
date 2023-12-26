b0f19829233b1beb89917d7bb3bb95ba2b08bfdb
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2023-12-26 11:04:44 +0900

    初期コミット

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "suspend_coroutine_already_resumed"
+include(":app")
